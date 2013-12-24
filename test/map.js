1c0012d1da5c8d323ea88c685d03355ca7f60387
James Halliday <mail@substack.net>
2013-12-24 15:38:42 -0800

    passing sparse map tests

--- test/map.js
+++ test/map.js
@@ -39,13 +39,33 @@ test('elements and indexes', function (t) {
     });
 });
 
-test('map ignores holes', function (t) {
+test('ignore holes', function (t) {
     map(new Array(5), function (x) {
         t.fail('stepped into a hole');
     });
     map(cripple(new Array(5)), function (x) {
         t.fail('stepped into a hole');
     });
+    t.end();
+});
+
+test('sparse map', function (t) {
+    t.plan(2);
+    var xs = new Array(5);
+    xs[2] = 'a';
+    xs[4] = 'b';
+    t.equal(
+        map(xs, function (x, i) { return x + i }).join(''),
+        'a2b4'
+    );
+    
+    var ys = new Array(5);
+    ys[2] = 'a';
+    ys[4] = 'b';
+    t.equal(
+        map(cripple(xs), function (x, i) { return x + i }).join(''),
+        'a2b4'
+    );
     t.end();
 });
 
