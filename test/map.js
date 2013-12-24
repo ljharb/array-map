91346e299b26734e70caf4e42ccc2e2e56ff9d10
James Halliday <mail@substack.net>
2013-12-24 15:31:49 -0800

    passing test for elements and indexes, failing test for holes

--- test/map.js
+++ test/map.js
@@ -14,6 +14,41 @@ test('numbers -> letters', function (t) {
     t.equal(b.join(''), 'abc');
 });
 
+test('elements and indexes', function (t) {
+    t.plan(8);
+    var x = { q: 5 }, y = 3, z = null;
+    
+    t.deepEqual(
+        map([x,y,z], function (c, i) { return i }),
+        [ 0, 1, 2 ],
+        'index check'
+    );
+    t.deepEqual(
+        map([x,y,z], function (c, i) { return i }),
+        [ 0, 1, 2 ],
+        'crippled index check'
+    );
+    
+    var xs0 = [ x, y, z ];
+    map(xs0, function (c, i, xs) {
+        t.strictEqual(xs, xs0, 'argument[2]');
+    });
+    var xs1 = [ x, y, z ];
+    map(xs1, function (c, i, xs) {
+        t.strictEqual(xs, xs1, 'crippled argument[2]');
+    });
+});
+
+test('map ignores holes', function (t) {
+    map(new Array(5), function (x) {
+        t.fail('stepped into a hole');
+    });
+    map(cripple(new Array(5)), function (x) {
+        t.fail('stepped into a hole');
+    });
+    t.end();
+});
+
 function cripple (xs) {
     xs.map = undefined;
     return xs;
