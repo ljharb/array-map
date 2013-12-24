5ee30eaf213f541716c8556f55b87c93c6cc5640
James Halliday <mail@substack.net>
2013-12-24 14:54:02 -0800

    cripple() function for 100% code coverage

--- test/map.js
+++ test/map.js
@@ -2,9 +2,19 @@ var map = require('../');
 var test = require('tape');
 
 test('numbers -> letters', function (t) {
-    t.plan(1);
-    var letters = map([97,98,99], function (c) {
+    t.plan(2);
+    var a = map([97,98,99], function (c) {
         return String.fromCharCode(c);
     });
-    t.equal(letters.join(''), 'abc');
+    t.equal(a.join(''), 'abc');
+    
+    var b = map(cripple([97,98,99]), function (c) {
+        return String.fromCharCode(c);
+    });
+    t.equal(b.join(''), 'abc');
 });
+
+function cripple (xs) {
+    xs.map = undefined;
+    return xs;
+}
