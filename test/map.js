3909421b325589bc27373575494c9a71337abdca
Jordan Harband <ljharb@gmail.com>
2022-10-12 00:41:45 -0700

    [eslint] add eslint

--- test/map.js
+++ test/map.js
@@ -1,14 +1,21 @@
+'use strict';
+
 var map = require('../');
 var test = require('tape');
 
+function cripple(xs) {
+    xs.map = undefined; // eslint-disable-line no-param-reassign
+    return xs;
+}
+
 test('numbers -> letters', function (t) {
     t.plan(2);
-    var a = map([97,98,99], function (c) {
+    var a = map([97, 98, 99], function (c) {
         return String.fromCharCode(c);
     });
     t.equal(a.join(''), 'abc');
-    
-    var b = map(cripple([97,98,99]), function (c) {
+
+    var b = map(cripple([97, 98, 99]), function (c) {
         return String.fromCharCode(c);
     });
     t.equal(b.join(''), 'abc');
@@ -16,19 +23,21 @@ test('numbers -> letters', function (t) {
 
 test('elements and indexes', function (t) {
     t.plan(8);
-    var x = { q: 5 }, y = 3, z = null;
-    
+    var x = { q: 5 };
+    var y = 3;
+    var z = null;
+
     t.deepEqual(
-        map([x,y,z], function (c, i) { return i }),
+        map([x, y, z], function (c, i) { return i; }),
         [ 0, 1, 2 ],
         'index check'
     );
     t.deepEqual(
-        map([x,y,z], function (c, i) { return i }),
+        map([x, y, z], function (c, i) { return i; }),
         [ 0, 1, 2 ],
         'crippled index check'
     );
-    
+
     var xs0 = [ x, y, z ];
     map(xs0, function (c, i, xs) {
         t.strictEqual(xs, xs0, 'argument[2]');
@@ -42,36 +51,27 @@ test('elements and indexes', function (t) {
 test('ignore holes', function (t) {
     t.plan(2);
     t.deepEqual(
-        map(new Array(5), function (x) { return x }),
+        map(new Array(5), function (x) { return x; }),
         []
     );
     t.deepEqual(
-        map(cripple(new Array(5)), function (x) { return x }),
+        map(cripple(new Array(5)), function (x) { return x; }),
         []
     );
 });
 
 test('sparse map', function (t) {
     t.plan(2);
-    var xs = new Array(5);
-    xs[2] = 'a';
-    xs[4] = 'b';
+    var xs = [,, 'a',, 'b']; // eslint-disable-line no-sparse-arrays
     t.equal(
-        map(xs, function (x, i) { return x + i }).join(''),
+        map(xs, function (x, i) { return x + i; }).join(''),
         'a2b4'
     );
-    
-    var ys = new Array(5);
-    ys[2] = 'a';
-    ys[4] = 'b';
+
+    var ys = [,, 'a',, 'b']; // eslint-disable-line no-sparse-arrays
     t.equal(
-        map(cripple(xs), function (x, i) { return x + i }).join(''),
+        map(cripple(ys), function (x, i) { return x + i; }).join(''),
         'a2b4'
     );
     t.end();
 });
-
-function cripple (xs) {
-    xs.map = undefined;
-    return xs;
-}
